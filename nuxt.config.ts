70b2f088aaac447c9dd134916dd5761fa14a81ff
aveeeeen <taichimatsumoto360@gmail.com>
2024-07-15 04:45:49 +0900

    wwwwww

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,9 +14,6 @@ export default defineNuxtConfig({
   alias: {
     "@": resolve(__dirname, "/")
   },
-  app: {
-    baseURL: '/index'
-  },
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2024-07-12',
   
