f533a0dd49a49177d292b5e0f0e4e0e5814e9cdb
aveeeeen <taichimatsumoto360@gmail.com>
2024-07-16 05:23:04 +0900

    added basic implementation of changing pages

--- pages/Notes.vue
+++ pages/Notes.vue
@@ -2,39 +2,41 @@
 const contentList = ref(null);
 const contentArrary = ref(null);
 const isUpdated = ref(false);
-const count = ref(0);
-const moveAmount = ref(0);
+const page = ref(0);
+const pages = ref(0);
 
 onMounted(async () => {
-  contentList.value = await queryContent('/').only(['title','_path','update','tags']).sort({update: -1, $numeric: true}).find()
-  console.log(contentList.value)
-  getContent(count.value,5)
-  moveAmount.value = Math.floor(contentList.value.length / 5);
-  console.log(moveAmount.value)
-})
+  contentList.value = await queryContent("/")
+    .only(["title", "_path", "update", "tags"])
+    .sort({ update: -1, $numeric: true })
+    .find();
+  console.log(contentList.value);
+  getContent(page.value, 5);
+  pages.value = Math.floor(contentList.value.length / 5);
+  console.log(pages.value);
+});
 
-async function getContent (start, move) {
-  contentArrary.value = await queryContent('/')
-    .only(['title','_path','update','tags'])
-    .sort({update: -1, $numeric: true})
+async function getContent(start, move) {
+  contentArrary.value = await queryContent("/")
+    .only(["title", "_path", "update", "tags"])
+    .sort({ update: -1, $numeric: true })
     .skip(start)
     .limit(move)
-    .find()
-  console.log(count.value);
+    .find();
+  console.log(page.value);
 }
 
-function getNextContent () {
-  if(count.value == moveAmount.value) return;
-  count.value ++;
-  getContent(count.value * 5, 5);
+function getNextContent() {
+  if (page.value == pages.value) return;
+  page.value++;
+  getContent(page.value * 5, 5);
 }
 
-function getPrevContent () {
-  if(count.value == 0) return;
-  count.value --;
-  getContent(count.value * 5, 5);
+function getPrevContent() {
+  if (page.value == 0) return;
+  page.value--;
+  getContent(page.value * 5, 5);
 }
-
 </script>
 
 <template>
@@ -42,21 +44,34 @@ function getPrevContent () {
     <div class="content-box">
       <h1>Notes</h1>
       <p>new → old</p>
-      <hr>
+      <hr />
       <ul v-for="content in contentArrary">
         <li>
           <NuxtLink :to="content._path">{{ content.title }}</NuxtLink>
-          <p>更新日: {{content.update.toString().slice(0,4) + "." + content.update.toString().slice(4,6) + "." + content.update.toString().slice(6,8) }}</p>
+          <p>
+            更新日:
+            {{
+              content.update.toString().slice(0, 4) +
+              "." +
+              content.update.toString().slice(4, 6) +
+              "." +
+              content.update.toString().slice(6, 8)
+            }}
+          </p>
         </li>
       </ul>
     </div>
   </div>
-  <div class="page-selector flex-hori">
-    <div>
-      <button @click="getPrevContent()">back</button>
-    <button @click="getNextContent()">next</button>
+  <div class="center--">
+    <div class="page-selector">
+      <div class="selector-flex center-">
+        <a class="" @click="getPrevContent()">back</a>
+        <p class=" page-num">{{ `${page + 1} / ${pages + 1}` }}</p>
+        <a class="" @click="getNextContent()">next</a>
+      </div>
     </div>
   </div>
+
   <div class="menu">
     <Menu></Menu>
   </div>
@@ -68,18 +83,30 @@ ul {
   display: block;
 }
 
-h2{
+h2 {
   text-decoration: none;
   font-size: 2rem;
 }
 
-.page-selector{
-  position: relative;
+.page-selector {
+  position: absolute;
+  bottom: 5svh;
 }
 
-button{
+.page-selector div{
+  margin: 5px;
+}
+
+button {
   width: 100px;
   background-color: gray;
   z-index: 3;
 }
+
+.selector-flex{
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+}
+
 </style>
