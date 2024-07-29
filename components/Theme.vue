bc6003065d9b77ab5b9a98c01e4f66ec8dee1ca0
aveeeeen <taichimatsumoto360@gmail.com>
2024-07-30 00:33:39 +0900

    added darkmode

---
+++ components/Theme.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="burger-circle">
+    <div>
+      <svg
+        id="darkmode"
+        viewBox="0 0 24 24"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
+          <g id="darkmode-icon" fill-rule="evenodd">
+            <path
+              d="M12,22 C17.5228475,22 22,17.5228475 22,12 C22,6.4771525 17.5228475,2 12,2 C6.4771525,2 2,6.4771525 2,12 C2,17.5228475 6.4771525,22 12,22 Z M12,20.5 L12,3.5 C16.6944204,3.5 20.5,7.30557963 20.5,12 C20.5,16.6944204 16.6944204,20.5 12,20.5 Z"
+            ></path>
+          </g>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#darkmode {
+  width: 2rem;
+  height: 2rem;
+  display: block;
+}
+
+#darkmode-icon{
+  fill: var(--text-color);
+}
+
+.burger-circle {
+  background-color: var(--ui-bg-color);
+  width: 2.5rem;
+  height: 2.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <script setup>
 const video = ref(null);
 const isVideoLoad = ref(false);
+const isDarkmode = ref(false);
+
 
 onMounted(() => {
   video.value = document.querySelector("video");
   video.value.load();
-
+  
   video.value.addEventListener("canplaythrough", (event) => {
     video.value.play();
     isVideoLoad.value = true;
@@ -32,6 +34,52 @@ watch(isVideoLoad, () => {
   if(!isVideoLoad.value) videoPlay();
   console.log(isVideoLoad.value);
 });
+
+function defaultTheme () {
+  const colorPalete = {
+    html:"blue",
+    bg: "rgb(245, 245, 255)",
+    uibg: "white",
+    text: "black",
+    a: "blue"
+  }
+  const r = document.querySelector(':root');
+  r.style.setProperty("--text-color", colorPalete.text);
+  r.style.setProperty("--text-color-a", colorPalete.a);
+  r.style.setProperty("--text-bg-color", colorPalete.bg);
+  r.style.setProperty("--text-bg-color-a", colorPalete.uibg);
+  r.style.setProperty("--ui-bg-color", colorPalete.uibg);
+  r.style.setProperty("--bg-color", colorPalete.bg);
+  document.querySelector("html").style.backgroundColor = colorPalete.html;
+}
+
+function darkmode () {
+  const colorPalete = {
+    html:"rgb(25,25,25)",
+    bg: "rgb(40,40,40)",
+    uibg: "rgb(100,100,100)",
+    abg: "rgb(10,10,10)"
+  }
+  const r = document.querySelector(':root');
+  r.style.setProperty("--text-color", "white");
+  r.style.setProperty("--text-color-a", "rgb(200,200,255)");
+  r.style.setProperty("--text-bg-color", colorPalete.bg);
+  r.style.setProperty("--text-bg-color-a", colorPalete.abg);
+  r.style.setProperty("--ui-bg-color", colorPalete.uibg);
+  r.style.setProperty("--bg-color",colorPalete.bg);
+  document.querySelector("html").style.backgroundColor = colorPalete.html;
+}
+
+watch(isDarkmode, async () => {
+  await nextTick();
+  if(isDarkmode.value){
+    darkmode();
+    console.log("is darkmode")
+  }else{
+    defaultTheme();
+    console.log("is not darkmode")
+  }
+});
 </script>
 
 <template>
@@ -50,7 +98,11 @@ watch(isVideoLoad, () => {
     </div>
 
     <div class="top-menu">
-      <TopMenu @click="isHomeClicked = !isHomeClicked"></TopMenu>
+      <TopMenu></TopMenu>
+    </div>
+
+    <div class="menu show-right">
+      <Theme @click="isDarkmode = !isDarkmode"></Theme>
     </div>
   
 </template>
@@ -62,7 +114,7 @@ watch(isVideoLoad, () => {
   position: fixed;
   top: 0%;
   left: 0%;
-  z-index: 0;
+  z-index: -1;
   object-fit: cover;
 }
 
