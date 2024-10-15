c7117e6e0ec10f633b93bff5ebc12cd57691c4f6
aveeeeen <taichimatsumoto360@gmail.com>
2024-10-15 15:51:04 +0900

    added loading skeleton

--- components/Skelton.vue
+++ components/Skelton.vue
@@ -9,12 +9,15 @@ import type { HTMLAttributes } from 'vue'
 <style scoped>
 
 .pulse {
-  animation: pulse-animation 1.8s infinite;
-  animation-timing-function: ease-in-out;
+  animation: pulse-animation 1.5s infinite;
+  animation-timing-function: ease-in;
 }
 
 @keyframes pulse-animation {
   100% {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+  50% {
     background-color: rgba(240, 240, 240, 1);
   }
   0% {
--- components/iframe/iframeSC.vue
+++ components/iframe/iframeSC.vue
@@ -18,14 +18,16 @@ const props = defineProps({
 </template>
 
 <style scoped>
-
 .pulse {
-  animation: pulse-animation 1.8s infinite;
-  animation-timing-function: ease-in-out;
+  animation: pulse-animation 1.5s infinite;
+  animation-timing-function: ease-in;
 }
 
 @keyframes pulse-animation {
   100% {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+  50% {
     background-color: rgba(240, 240, 240, 1);
   }
   0% {
