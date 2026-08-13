f401f769715866110eba7b81cb8a565effd7a760
aveeeeen <58681310+aveeeeen@users.noreply.github.com>
2026-08-13 17:30:46 +0900

    Update Open Graph and Twitter image URLs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,13 +22,13 @@ export default defineNuxtConfig({
         { property: 'og:title', content: 'portfolio site of braven' },
         { property: 'og:description', content: '活動履歴やブログをまとめています。' },
         { property: 'og:url', content: 'https://braveeeeen.vercel.app/' },
-        { property: 'og:image', content: 'https://braveeeeen.vercel.app/ogp.png' }, // 絶対パスで指定
+        { property: 'og:image', content: 'https://raw.githubusercontent.com/aveeeeen/portfolio/refs/heads/main/public/ogp.png' }, // 絶対パスで指定
 
         // X (Twitter) Card 共通設定
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:title', content: 'portfolio site of braven' },
         { name: 'twitter:description', content: '活動履歴やブログをまとめています。' },
-        { name: 'twitter:image', content: 'https://braveeeeen.vercel.app/ogp.png' },
+        { name: 'twitter:image', content: 'https://raw.githubusercontent.com/aveeeeen/portfolio/refs/heads/main/public/ogp.png' },
       ],
     },
   },
